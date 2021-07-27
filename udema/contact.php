60665fb08c696b42095e2415a98e35839efc4133
muhammad kumail <muhammadkomail197@gmail.com>
2021-07-27 22:24:05 +0500

    UDEMA

---
+++ udema/contact.php
@@ -0,0 +1,390 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Udema a modern educational site template">
+    <meta name="author" content="Ansonika">
+    <title>UDEMA | Modern Educational site template</title>
+
+    <!-- Favicons-->
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" type="image/x-icon" href="img/apple-touch-icon-57x57-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="img/apple-touch-icon-72x72-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="114x114"
+        href="img/apple-touch-icon-114x114-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="144x144"
+        href="img/apple-touch-icon-144x144-precomposed.png">
+
+    <!-- GOOGLE WEB FONT -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap"
+        rel="stylesheet">
+
+    <!-- BASE CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/vendors.css" rel="stylesheet">
+    <link href="css/icon_fonts/css/all_icons.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- YOUR CUSTOM CSS -->
+    <link href="css/custom.css" rel="stylesheet">
+
+    <script type="text/javascript">
+      var onloadCallback = function() {
+        grecaptcha.render('html_element', {
+          'sitekey' : '6LdMGfoZAAAAAE4CPeu9mJ5rmosUgsi3DFftAXk0'
+        });
+      };
+    </script>
+
+</head>
+
+<body>
+<style>
+    a{
+        text-decoration: none; 
+    }
+</style>
+    <div id="page">
+
+        <header class="header menu_2">
+            <div id="preloader">
+                <div data-loader="circle-side"></div>
+            </div><!-- /Preload -->
+            <div id="logo">
+                <a href="index.html"><img src="img/logo.png" width="149" height="42" alt=""></a>
+            </div>
+            <ul id="top_menu">
+                <li><a href="login.html" class="login">Login</a></li>
+                <li><a href="index.html#0" class="search-overlay-menu-btn">Search</a></li>
+                <li class="hidden_tablet"><a href="admission.html" class="btn_1 rounded">Admission</a></li>
+            </ul>
+            <!-- /top_menu -->
+            <a href="index.html#menu" class="btn_mobile">
+                <div class="hamburger hamburger--spin" id="hamburger">
+                    <div class="hamburger-box">
+                        <div class="hamburger-inner"></div>
+                    </div>
+                </div>
+            </a>
+            <nav id="menu" class="main-menu">
+                <ul>
+                    <li><span><a href="index.html#0">Home</a></span>
+                        <ul>
+                            <li><a href="index.html">Home version 1</a></li>
+                            <li><a href="index-2.html">Home version 2</a></li>
+                            <li><a href="index-6.html">Home version 3</a></li>
+                            <li><a href="index-3.html">Home version 4</a></li>
+                            <li><a href="index-4.html">Home version 5</a></li>
+                            <li><a href="index-5.html">With Cookie bar (EU law)</a></li>
+                        </ul>
+                    </li>
+                    <li><span><a href="index.html#0">Courses</a></span>
+                        <ul>
+                            <li><a href="courses-grid.html">Courses grid</a></li>
+                            <li><a href="courses-grid-sidebar.html">Courses grid sidebar</a></li>
+                            <li><a href="courses-list.html">Courses list</a></li>
+                            <li><a href="courses-list-sidebar.html">Courses list sidebar</a></li>
+                            <li><a href="course-detail.php">Course detail</a></li>
+                            <li><a href="course-detail-2.html">Course detail working form</a></li>
+                            <li><a href="admission.html">Admission wizard</a></li>
+                            <li><a href="teacher-detail.html">Teacher detail</a></li>
+                        </ul>
+                    </li>
+                    <li><span><a href="index.html#0">Pages</a></span>
+                        <ul>
+                            <li><a href="menu_2/index.html">Menu 2</a></li>
+                            <li><a href="about.html">About</a></li>
+                            <li><a href="blog.html">Blog</a></li>
+                            <li><a href="login.html">Login</a></li>
+                            <li><a href="register.html">Register</a></li>
+                            <li><a href="contacts.html">Contacts</a></li>
+                            <li><a href="404.html">404 page</a></li>
+                            <li><a href="agenda-calendar.html">Agenda Calendar</a></li>
+                            <li><a href="faq.html">Faq</a></li>
+                            <li><a href="help.html">Help</a></li>
+                        </ul>
+                    </li>
+                    <li><span><a href="index.html#0">Extra Pages</a></span>
+                        <ul>
+                            <li><a href="media-gallery.html">Media gallery</a></li>
+                            <li><a href="admin_section/index.html" target="_blank">Admin-Dashboard
+                                    <strong>New!</strong></a></li>
+                            <li><a href="cart-1.html">Cart page 1</a></li>
+                            <li><a href="cart-2.html">Cart page 2</a></li>
+                            <li><a href="cart-3.html">Cart page 3</a></li>
+                            <li><a href="pricing-tables.html">Responsive pricing tables</a></li>
+                            <li><a href="coming_soon/index.html">Coming soon</a></li>
+                            <li><a href="icon-pack-1.html">Icon pack 1</a></li>
+                            <li><a href="icon-pack-2.html">Icon pack 2</a></li>
+                            <li><a href="icon-pack-3.html">Icon pack 3</a></li>
+                            <li><a href="icon-pack-4.html">Icon pack 4</a></li>
+                        </ul>
+                    </li>
+                    <li><span><a href="https://1.envato.market/Gk0PV" target="_parent">Buy template</a></span></li>
+                    <li><span><a href="contact.php" target="_parent">Contact</a></span></li>
+                </ul>
+            </nav>
+            <!-- Search Menu -->
+            <div class="search-overlay-menu">
+                <span class="search-overlay-close"><span class="closebt"><i class="ti-close"></i></span></span>
+                <form role="search" id="searchform" method="get">
+                    <input value="" name="q" type="search" placeholder="Search..." />
+                    <button type="submit"><i class="icon_search"></i>
+                    </button>
+                </form>
+            </div><!-- End Search Menu -->
+        </header>
+        <!-- /header -->
+
+        <main>
+            <section class="hero_single version_2">
+                <div class="wrapper">
+                    <div class="container">
+                        <h3>What would you learn?</h3>
+                        <p>Increase your expertise in business, technology and personal development</p>
+                        <form>
+                            <div id="custom-search-input">
+                                <div class="input-group">
+                                    <input type="text" class=" search-query"
+                                        placeholder="Ex. Architecture, Specialization...">
+                                    <input type="submit" class="btn_search" value="Search">
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </section>
+            <!-- /hero_single -->
+            <br><br><br>
+            <div>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3915.8561363961617!2d76.990635!3d11.049411!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe1afc0945f8f419b!2sSpark%20Databox!5e0!3m2!1sen!2sus!4v1627200822517!5m2!1sen!2sus"
+                            style="border:0; position: relative; width: 75%; height: 600px; background-color: #CCC;
+                        margin: 1% 0 0 15%;" allowfullscreen="" loading="lazy"></iframe>
+                    </div>
+                    <style>
+                        input,
+                        textarea {
+                            margin: 10px;
+                            padding-top: 10px;
+                            padding-bottom: 10px;
+                            border: none;
+                            outline: none;
+                            border-bottom: 2px solid black;
+                            background: transparent;
+                        }
+
+                        /* input:hover{
+                            border: none;
+                        } */
+                        button:hover {
+                            background-color: blue;
+                            color: white;
+                        }
+                    </style>
+
+<!-- ************************************************************************************* -->
+
+<?php 
+
+include 'config.php';
+
+error_reporting(0);
+
+session_start();
+
+if (isset($_SESSION['fname'])) {
+    header("Location: index.html");
+}
+
+if (isset($_POST['submit'])) {
+	$fname = $_POST['fname'];
+	$email = $_POST['email'];
+	$pnum = $_POST['pnum'];
+	$desc = $_POST['desc'];
+
+
+
+    if(isset($_POST['g-recaptcha-response']))
+    {
+        $recaptcha = $_POST['g-recaptcha-response'];
+
+        if(!$recaptcha){
+            echo "<script>alert('Please go back and check recaptcha')</script>"; 
+            header("Location: contact.php");
+        }
+        else{
+        $secretkey = "6LdMGfoZAAAAAE4CPeu9mJ5rmosUgsi3DFftAXk0";
+        $url = "https://www.google.com/recaptcha/api/siteverify?
+        secret= '.$secretkey.' &response='.$recaptcha.'&remoteip= $UserIP";
+        $response = file_get_contents ($url);
+        $responsekey = json_decode($response,true);
+
+        // if($responsekey['success']){
+            
+		$sql = "SELECT * FROM message WHERE fname ='$fname'";
+		
+        $result = mysqli_query($conn, $sql);
+		
+			$sql = "INSERT INTO `message` (`fname`, `email`, `pnum`, `desc`)
+             VALUES ('$fname', '$email', '$pnum', '$desc');";
+			
+            $result = mysqli_query($conn, $sql);
+            
+        
+			if($result)
+            {
+				echo "<script>alert('Thank you for your feedback')</script>";
+				$fname = "";
+				$email = "";
+				$pnum = "";
+				$desc = "";
+
+            }
+        }
+    
+}
+else{
+    echo "<script>alert('Invalid Captcha, please try again')</script>";
+    header("Location: contact.php");
+}
+
+		
+	} 
+
+
+?>
+
+
+<!-- ************************************************************************************** -->
+           <div class="col-md-6">
+                        <div class="info" style="
+                        outline: none;
+                        font-family: Roboto, Arial, sans-serif;
+                        font-size: 16px;
+                        padding-top: 50px;
+                        display: flex;
+                        flex-direction: column;
+                        border: none;
+                        margin-right: 50px;">
+                            <h2 class="black mb-4">Send a message</h2>
+                        <form action="" method="POST">
+                            <input class="fname" type="text" name="fname" placeholder="Full name" required>
+                            <input type="text" name="email" placeholder="Email" required>
+                            <input type="text" name="pnum" placeholder="Phone number" required>
+                            <textarea name="desc" id="desc" cols="30" rows="5" placeholder="Your message" required></textarea>
+                            <style>
+                            /* .recap{
+                                margin-top:15px;
+                                margin-bottom:20px;
+                                
+                            } */
+                            </style>
+                            <div id="html_element" class="g-recaptcha"></div>
+                            <div style="margin-top:70px;">
+                            <button name="submit" class="btn btn-primary">Submit</button>
+                            <a class="btn btn-primary" href="https://www.google.com/maps/place/Spark+Databox/@11.0505379,76.955648,13z/data=!4m5!3m4!1s0x0:0xe1afc0945f8f419b!8m2!3d11.0505399!4d76.9906676?hl=en"target="_blank">Get Direction</a>
+                                                <a href="tel:+612380933400" class="btn btn-primary">Call US</a>
+                            <a href="mailto:komailkhan235@gmail.com" class="btn btn-primary"> Email Us</a>
+                            </div>
+                        </form>
+                        </div>
+                    </div>
+                </div>
+<!-- ************************************************************************************** -->
+                
+            </div>
+
+
+            <br><br><br><br><br>
+            <footer>
+                <div class="container margin_120_95">
+                    <div class="row justify-content-between">
+                        <div class="col-lg-5 col-md-12">
+                            <p><img src="img/logo.png" width="149" height="42" alt=""></p>
+                            <p>Mea nibh meis philosophia eu. Duis legimus efficiantur ea sea. Id placerat tacimates
+                                definitionem sea, prima quidam vim no. Duo nobis persecuti cu. Nihil facilisi indoctum
+                                an vix, ut delectus expetendis vis.</p>
+                            <div class="follow_us">
+                                <ul>
+                                    <li>Follow us</li>
+                                    <li><a href="index.html#0"><i class="ti-facebook"></i></a></li>
+                                    <li><a href="index.html#0"><i class="ti-twitter-alt"></i></a></li>
+                                    <li><a href="index.html#0"><i class="ti-google"></i></a></li>
+                                    <li><a href="index.html#0"><i class="ti-pinterest"></i></a></li>
+                                    <li><a href="index.html#0"><i class="ti-instagram"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 ml-lg-auto">
+                            <h5>Useful links</h5>
+                            <ul class="links">
+                                <li><a href="index.html#0">Admission</a></li>
+                                <li><a href="index.html#0">About</a></li>
+                                <li><a href="index.html#0">Login</a></li>
+                                <li><a href="index.html#0">Register</a></li>
+                                <li><a href="index.html#0">News &amp; Events</a></li>
+                                <li><a href="index.html#0">Contacts</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <h5>Contact with Us</h5>
+                            <ul class="contacts">
+                                <li><a href="tel://61280932400"><i class="ti-mobile"></i> + 61 23 8093 3400</a></li>
+                                <li><a href="mailto:info@udema.com"><i class="ti-email"></i> info@udema.com</a></li>
+                            </ul>
+                            <div id="newsletter">
+                                <h6>Newsletter</h6>
+                                <div id="message-newsletter"></div>
+                                <form method="post" action="assets/newsletter.htm" name="newsletter_form"
+                                    id="newsletter_form">
+                                    <div class="form-group">
+                                        <input type="email" name="email_newsletter" id="email_newsletter"
+                                            class="form-control" placeholder="Your email">
+                                        <input type="submit" value="Submit" id="submit-newsletter">
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /row -->
+                    <hr>
+                    <div class="row">
+                        <div class="col-md-8">
+                            <ul id="additional_links">
+                                <li><a href="index.html#0">Terms and conditions</a></li>
+                                <li><a href="index.html#0">Privacy</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-md-4">
+                            <div id="copy">© Udema</div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <!--/footer-->
+    </div>
+    <!-- page -->
+
+    <!-- COMMON SCRIPTS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="js/jquery-3.6.0.min.js"></script>
+    <script src="js/common_scripts.js"></script>
+    <script src="js/main.js"></script>
+    <script src="assets/validate.js"></script>
+    <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
+        async defer>
+    </script>
+
+</body>
+
+</html>
